01fd9ff761400a89db9b1f1afa88f9203a087426
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-11-21 15:57:24 -0500

    Message logged with details.

--- main.js
+++ main.js
@@ -1,71 +1,77 @@
 function main(){
     var rowIndex = 2 // Header is skipped
     for (const rowData of DATA) {
-      // Gather Variables
-      task = getCellData(rowData, headers.task)
-      dueDate = getCellData(rowData, headers.dueDate)
-      daysNotice = getCellData(rowData, headers.daysNotice)
-  
-      reminder = reminderRequired(task ,dueDate, daysNotice)
-      if (reminderRequired(task, dueDate, daysNotice)){
-        //logTaskReminders(rowData, task);
-        postTaskReminders(rowData, task);
+      var reminderType = getReminderType(rowData);
+      if (reminderType){
+        logTaskReminders(rowData, reminderType);
       }
       rowIndex ++ 
     }
   }
   
-  
-  function reminderRequired(task, dueDate, daysNotice) {
-    // Check that a reminder timeframe is possible.
-    if (task === "" || daysNotice === "") {
-      return false;
-    }
-    if (!(dueDate instanceof Date) || isNaN(daysNotice)) {
-      return false;
-    }
-  
-    // Set the relevant dates
-    var currentDate = new Date();
-    currentDate.setHours(0, 0, 0, 0);
-    var dueDate = new Date(dueDate);
-    dueDate.setHours(0, 0, 0, 0)
-    var reminderDate = new Date();
-    reminderDate.setDate(dueDate.getDate() - daysNotice);
-    reminderDate.setHours(0, 0, 0, 0)
 
-    // Return Statement
-    switch (true) {
-        case dueDate < currentDate:
-            return "Overdue";
-        case dueDate.getTime() === currentDate.getTime():
-            return "Due Today";
-        case currentDate.getTime() === reminderDate.getTime():
-            return "Due Soon";
-        default:
-            return false;
-    }
-  };
-  
+function getReminderType(rowData){
+  // Get Data
+  task = getCellData(rowData, headers.task)
+  dueDate = getCellData(rowData, headers.dueDate)
+  daysNotice = getCellData(rowData, headers.daysNotice)
 
-  function logTaskReminders(rowData, task){
-    // Post reminders to console
-    rowData.slice(3).forEach((status, index) => {
-      if (!NO_REMINDER.includes(status.trim())) {
-        var originalIndex = index +3
-        console.log("REMINDER", getEmployee(originalIndex), task, status)
-      }
-    });
+  // Determine if valid reminder is possible
+  if (task === "" || daysNotice === "") {
+    return false;
   };
-  
- 
+  if (!(dueDate instanceof Date) || isNaN(daysNotice)) {
+    return false;
+  };
 
-  function sendReminders(rowData, task){
+  // Set the relevant dates
+  var currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0);
+  var dueDate = new Date(dueDate);
+  dueDate.setHours(0, 0, 0, 0);
+  var reminderDate = new Date();
+  reminderDate.setDate(dueDate.getDate() - daysNotice);
+  reminderDate.setHours(0, 0, 0, 0);
 
+  // Return Statement
+  switch (true) {
+    case dueDate < currentDate:
+        return "Overdue";
+    case dueDate.getTime() === currentDate.getTime():
+        return "Due Today";
+    case currentDate.getTime() === reminderDate.getTime():
+        return "Due Soon";
+    default:
+        return false;
   };
-  
-  function getEmployee(columnIndex){
-    // The current iteration of cells in a row should match the header indices.
-    // Therefore, the employee name can be found by taking the index of all headers.
-    return HEADERROW[columnIndex]
-  };
+};
+
+
+
+function logTaskReminders(rowData, reminderType){
+  // Get Data
+  task = getCellData(rowData, headers.task)
+  dueDate = getCellData(rowData, headers.dueDate)
+  const sheetURL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}`;
+
+
+  rowData.slice(3).forEach((status, index) => {
+    if (!NO_REMINDER.includes(status.trim())) {
+      var employee = _getEmployee(index+3, status)
+      //var email = emailDict[employee]
+      console.log("TARGET", employee)
+      console.log("TITLE", task, reminderType)
+      console.log(`BODY ${task} is due on ${removeTime(dueDate)}. Please make sure to get this completed. Link: ${sheetURL}`);    };
+  });
+};
+
+
+function _getEmployee(columnIndex){
+// The current iteration of cells in a row should match the header indices.
+// Therefore, the employee name can be found by taking the index of all headers.
+return HEADERROW[columnIndex]
+};
+
+
+
+// ToDo - don't hardcode the due messages. 
