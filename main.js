1405d8e0b9b21de7a2c18b17e38274fafb4e4992
ShinyMyst <122401790+ShinyMyst@users.noreply.github.com>
2023-11-21 15:25:32 -0500

    Revised remidner function

--- main.js
+++ main.js
@@ -1,58 +1,71 @@
 function main(){
-  var rowIndex = 2 // Header is skipped
-  for (const row of DATA) {
-    // Gather Data
-    task = getCellData(row, headers.task)
-    dueDate = getCellData(row, headers.dueDate)
-    daysNotice = getCellData(row, headers.daysNotice)
-
-    if (reminderRequired(task, dueDate, daysNotice)){
-      sendTaskReminders(row, task);
-      postReminderComment(cell);
+    var rowIndex = 2 // Header is skipped
+    for (const rowData of DATA) {
+      // Gather Variables
+      task = getCellData(rowData, headers.task)
+      dueDate = getCellData(rowData, headers.dueDate)
+      daysNotice = getCellData(rowData, headers.daysNotice)
+  
+      reminder = reminderRequired(task ,dueDate, daysNotice)
+      if (reminderRequired(task, dueDate, daysNotice)){
+        //logTaskReminders(rowData, task);
+        postTaskReminders(rowData, task);
+      }
+      rowIndex ++ 
     }
-    rowIndex ++
   }
-}
-
-
-function postReminderComment(cell){
-  var currentComment = sheet.getRange()
-}
-
-
-function reminderRequired(task, dueDate, daysNotice) {
-  // Check that a reminder timeframe is possible.
-  if (task === "" || daysNotice === "") {
-    return false;
-  }
-  if (!(dueDate instanceof Date) || isNaN(daysNotice)) {
-    return false;
-  }
-
-  // Determine if we fall within the reminder time frame
-  var currentDate = new Date();
-  var dueDateObj = new Date(dueDate);
-  var reminderDate = new Date();
-  reminderDate.setDate(currentDate.getDate() - daysNotice);
-
-  return reminderDate <= currentDate && dueDateObj <= currentDate;
-};
-
-function sendTaskReminders(row, task){
-  row.slice(3).forEach((status, index) => {
-    if (!noReminder.includes(status.trim())) {
-      var originalIndex = index +3
-      console.log("REMINDER", getEmployee(originalIndex), task, status)
+  
+  
+  function reminderRequired(task, dueDate, daysNotice) {
+    // Check that a reminder timeframe is possible.
+    if (task === "" || daysNotice === "") {
+      return false;
     }
-  });
-};
+    if (!(dueDate instanceof Date) || isNaN(daysNotice)) {
+      return false;
+    }
+  
+    // Set the relevant dates
+    var currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    var dueDate = new Date(dueDate);
+    dueDate.setHours(0, 0, 0, 0)
+    var reminderDate = new Date();
+    reminderDate.setDate(dueDate.getDate() - daysNotice);
+    reminderDate.setHours(0, 0, 0, 0)
 
-function postComment(){
+    // Return Statement
+    switch (true) {
+        case dueDate < currentDate:
+            return "Overdue";
+        case dueDate.getTime() === currentDate.getTime():
+            return "Due Today";
+        case currentDate.getTime() === reminderDate.getTime():
+            return "Due Soon";
+        default:
+            return false;
+    }
+  };
+  
 
-}
+  function logTaskReminders(rowData, task){
+    // Post reminders to console
+    rowData.slice(3).forEach((status, index) => {
+      if (!NO_REMINDER.includes(status.trim())) {
+        var originalIndex = index +3
+        console.log("REMINDER", getEmployee(originalIndex), task, status)
+      }
+    });
+  };
+  
+ 
 
-function getEmployee(index){
-  // The current iteration of cells in a row should match the header indices.
-  // Therefore, the employee name can be found by taking the index of all headers.
-  return HEADERROW[index]
-};
+  function sendReminders(rowData, task){
+
+  };
+  
+  function getEmployee(columnIndex){
+    // The current iteration of cells in a row should match the header indices.
+    // Therefore, the employee name can be found by taking the index of all headers.
+    return HEADERROW[columnIndex]
+  };
